34cb5a38c6395527c5935a5422a357690786f4c4
Daria <kordesovad@gmail.com>
2025-04-21 13:00:47 +0200

    add task status toggle from frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,16 +9,45 @@ function App() {
       .then(data => setTasks(data));
   }, []);
 
+  function setTaskStatus (taskId) {
+    console.log("changing task status... ", taskId);
+    fetch(`http://localhost:3008/tasks/${taskId}`)
+      .then(res => res.json())
+      .then( task => {
+        let newStatus = !task.completed
+        return fetch(`http://localhost:3008/tasks/${taskId}`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ completed: newStatus })
+        });
+      })
+      .then(() => {
+        return fetch('http://localhost:3008/tasks')
+          .then(res => res.json())
+          .then(data => setTasks(data));
+      })
+      .catch(err => console.error("Error while updating task:", err));
+  }
+
   return (
-    <div>
+    <div className='mainContainer'>
       <h1>Tasks:</h1>
-      <ul>
-        {tasks.map(task => (
-          <li key={task.id}>
-            {task.title} — {task.completed ? '✅' : '❌'}
-          </li>
+      <div className="AllTasksContainer">
+
+
+        {tasks.map(task => (        
+          <div className="taskContainer" key={task.id}>
+          <input type="checkbox" checked={task.completed} onChange={() => setTaskStatus(task.id)}/>
+          <label htmlFor="vehicle1"> {task.title} </label>
+          </div>
         ))}
-      </ul>
+
+
+      </div>
+
+
     </div>
   );
 }
